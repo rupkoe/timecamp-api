4ae6c0c26b4e9fa9c9897866fc20791d3d1c2254
dfritschy <donat@fritschy.ch>
2021-02-09 16:01:54 +0100

    wip very much so: tree traversal keeping parent ids, some pieces for summarization

--- parser/project.go
+++ parser/project.go
@@ -1,10 +1,12 @@
 package parser
 
 import (
+	"errors"
 	"fmt"
 	"github.com/rupkoe/timecamp-api"
 	"sort"
 	"strings"
+	"time"
 )
 
 type ProjectNode struct {
@@ -21,6 +23,37 @@ func GetProjectList(tasks []api.Task) []api.Task {
 		}
 	}
 	return result
+}
+
+func GetTaskById(tasks []api.Task, id string) (*api.Task, error) {
+	for _, task := range tasks {
+		if task.TaskID == id {
+			return &task, nil
+		}
+	}
+	return nil, errors.New("task with ID " + id + " not found")
+}
+
+type Totals struct {
+	TotalTime    time.Duration
+	BillableTime time.Duration
+}
+
+func (t Totals) add(total Totals) Totals {
+	t.BillableTime = t.BillableTime + total.BillableTime
+	t.TotalTime = t.TotalTime + total.TotalTime
+	return t
+}
+
+type TaskTotals map[string]Totals
+
+func (tt TaskTotals) add(taskId string, totals Totals) {
+	currentTotals, prs := tt[taskId]
+	if !prs {
+		tt[taskId] = totals
+	} else {
+		tt[taskId] = currentTotals.add(totals)
+	}
 }
 
 // GetProjects returns projects and corresponding tasks, sorted alphabetically.
@@ -51,22 +84,44 @@ func GetProjectTree(tasks []api.Task) ([]ProjectNode, error) {
 
 // https://stackoverflow.com/questions/22957638/make-a-tree-from-a-table-using-golang
 
-func PrintProjectTree(tasks []api.Task, project api.Task) {
-	TraverseTree(tasks, project, printit)
-}
+var parentIds = make(map[int]string)
 
-func printit(task api.Task) {
-	for i := 1; i < task.LevelParsed(); i++ {
-		fmt.Print("--")
-	}
-	fmt.Println(task.Level, task.Name)
-}
-
-func TraverseTree(tasks []api.Task, parent api.Task, callback func(api.Task)) {
+func TraverseTree(tasks []api.Task, parent api.Task, callback func(api.Task, map[int]string)) {
 	for _, task := range tasks {
 		if task.ParentID == parent.TaskID {
-			callback(task)
+			parentIds[task.LevelParsed()-1] = task.ParentID
+			callback(task, parentIds)
 			TraverseTree(tasks, task, callback)
+			delete(parentIds, task.LevelParsed()-1)
 		}
 	}
+}
+
+func Summarize(tasks []api.Task, entries []api.TimeEntry, root api.Task) {
+	TraverseTree(tasks, root, func(task api.Task, parentIds map[int]string) {
+		timeEntries := GetEntriesForTask(entries, task.TaskID)
+		var taskTimes Totals
+		for _, timeEntry := range timeEntries {
+			duration, _ := timeEntry.DurationParsed()
+			if timeEntry.IsBillable() {
+				taskTimes.BillableTime += duration
+			} else {
+				taskTimes.TotalTime += duration
+			}
+
+		}
+		fmt.Printf("Total for Task %v: Total %v, Billable %v", task.TaskID, taskTimes.TotalTime, taskTimes.BillableTime)
+		fmt.Println()
+		// todo: add to TaskTotals based on the provided parentIds array
+	})
+}
+
+func GetEntriesForTask(entries []api.TimeEntry, taskID string) []api.TimeEntry {
+	var result []api.TimeEntry
+	for _, entry := range entries {
+		if entry.TaskID == taskID {
+			result = append(result, entry)
+		}
+	}
+	return result
 }
